38a9a706ce05aa3c881916d3a05b1cf03eeb9cb1
yerko33 <yerko.guerra.22@gmail.com>
2025-02-22 10:40:24 -0300

    Crea funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo_nuevo = document.getElementById('amigo').value;
+    
+    if(amigo_nuevo == ""){
+        alert('Por favor, inserte un nombre.');
+    }
+    else{
+        amigos.push(amigo_nuevo);
+        document.querySelector('#amigo').value = '';
+        listaAmigos();
+    }
+}
+
